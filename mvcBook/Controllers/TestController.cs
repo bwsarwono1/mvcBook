66d628735ea60f04b9144c6bfd5b61c292ffb8a4
bsarwono1 <wiwidsarwono@aol.com>
2022-04-28 14:30:34 -0500

    New test view

---
+++ mvcBook/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace mvcBook.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult TestView()
+        {
+            return View();
+        }
+    }
+
+   
+}
